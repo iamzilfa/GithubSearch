c49a0e18c749bfd090cceca83d87842d47fcdd56
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-07 19:42:01 +0200

    added data in user.ts

--- src/app/user-request.service.ts
+++ src/app/user-request.service.ts
@@ -1,10 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserRequestService {
 
-  
-  constructor() { }
+  user: User;
+
+  constructor(private http:HttpClient) { 
+    this.user = new User("","","",0);
+  }
+
+  quoteRequest(){
+    interface ApiResponse{
+      avatar_url:string;
+      name:string;
+      login:string;
+      public_repos:number
+    }
+    let promise = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+        this.user.avatar_url = response.avatar_url
+        this.user.name = response.name
+        this.user.login = response.login
+        this.user.public_repos = response.public_repos
+
+        resolve()
+      },
+      error=>{
+        this.user.name = "Hello Dear"
+        this.user.login = "Enter your url"
+
+        reject(error)
+      })
+    })
+    return promise
+  }
+
 }
