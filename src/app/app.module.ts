964a6aebd0f47dd1cd10eb5e48e370d6358b7896
Zilfa Cyamani <zilcyam@gmail.com>
2020-11-07 16:52:28 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { GithubFormComponent } from './github-form/github-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoryComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    GithubFormComponent
   ],
   imports: [
     BrowserModule,
